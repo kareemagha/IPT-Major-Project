a8c3a10ab71595c1ef91738c262b3163b05772a5
Kareem <kareemagha100@gmail.com>
2023-05-24 20:57:54 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function () {
+    alert("Hello Ms\nWhile marking this, please adjust your zoom. On my screen (16:9), I found that 67% - 75% zoom worked best\n\nRegards,\nKareem Agha")
+  })
